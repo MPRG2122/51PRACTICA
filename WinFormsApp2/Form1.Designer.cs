f012678afa6e03600b41f76b36fcfe1e188d3a7e
MPRGDAW1920 <rodes20257@iesmarenostrum.com>
2022-02-25 15:33:29 +0100

    Agrego prueba formulario

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aluNombre = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.TextBox();
+            this.listaAlumnos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // aluNombre
+            // 
+            this.aluNombre.Location = new System.Drawing.Point(182, 22);
+            this.aluNombre.Name = "aluNombre";
+            this.aluNombre.Size = new System.Drawing.Size(100, 23);
+            this.aluNombre.TabIndex = 0;
+            // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(417, 13);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(100, 23);
+            this.aluNota.TabIndex = 1;
+            // 
+            // listaAlumnos
+            // 
+            this.listaAlumnos.Location = new System.Drawing.Point(286, 89);
+            this.listaAlumnos.Multiline = true;
+            this.listaAlumnos.Name = "listaAlumnos";
+            this.listaAlumnos.ReadOnly = true;
+            this.listaAlumnos.Size = new System.Drawing.Size(100, 23);
+            this.listaAlumnos.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listaAlumnos);
+            this.Controls.Add(this.aluNota);
+            this.Controls.Add(this.aluNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox aluNombre;
+        private System.Windows.Forms.TextBox aluNota;
+        private System.Windows.Forms.TextBox listaAlumnos;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
